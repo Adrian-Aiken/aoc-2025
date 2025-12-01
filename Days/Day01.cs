40389c68f6836995fc28c9acae149b4749a85067
Adrian Aiken <adaiken@outlook.com>
2025-11-30 23:51:32 -0800

    Day 1

--- Days/Day01.cs
+++ Days/Day01.cs
@@ -2,19 +2,54 @@ namespace AOC.Y2025
 {
     public class Day01 : IProblem
     {
+        private List<string> rotations = new List<string>();
+        private int position = 50, zeros, passes;
+
         public void Parse(string[] input)
         {
-
+            rotations.AddRange(input);
         }
 
         public object PartOne()
         {
-            return -1;
+            DoRotations();
+            return zeros;
         }
 
         public object PartTwo()
         {
-            return -1;
+            DoRotations();
+            return passes;
+        }
+
+        private void DoRotations()
+        {
+            if (passes != 0) return;
+
+            foreach (string rotation in rotations)
+            {
+                int distance = int.Parse(rotation.Substring(1));
+
+                if (rotation[0] == 'R')
+                {
+                    for (int i = 0; i < distance; i++)
+                    {
+                        position++;
+                        if (position == 100) { position = 0; passes++; }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < distance; i++)
+                    {
+                        position--;
+                        if (position == 0) passes++;
+                        else if (position == -1) position = 99;
+                    }
+                }
+
+                if (position == 0) zeros++;
+            }
         }
     }
 }
